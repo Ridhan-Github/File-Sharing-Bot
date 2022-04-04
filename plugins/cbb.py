3869552a84c84ce714f85a970c01b61ae9c66058
Ridhan-Github <100027036+Ridhan-Github@users.noreply.github.com>
2022-04-04 19:50:49 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Disney Bots</a>\n○ Language : <code>Python</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram</a>\n○ Source Code : <a href='https://t.me/tamilanbots'>Click here</a>\n○ Channel : @Disney_Bots\n○ Support Group : @Disneybots_Support</b>",
+            text = f"<b>○ Creator : <a href='https://Disney_Bots'>Disney Bots</a>\n○ Language : <code>Python</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram</a>\n○ Source Code : <a href='https://t.me/tamilanbots'>Click here</a>\n○ Channel : @Disney_Bots\n○ Support Group : @Disneybots_Support</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
